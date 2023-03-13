506a8fc4634b3eb48da3d4e4ad81e37801fb104f
Long Dinh <longinh805@gmail.com>
2023-03-13 21:36:39 +0700

    Frontend

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("xin chao")
+const handleLogin = (event) => {
+  console.log(event);
+};
+
+let form = document.querySelector(".form");
+
+form.addEventListener("submit", e => {
+  console.log(document.getElementsByName("email"));
+  e.preventDefault();
+});
