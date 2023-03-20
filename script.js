fbf0bd7071aa83b4f533778095452de353444122
Long Dinh <longinh805@gmail.com>
2023-03-20 22:38:41 +0700

    CREDENTIAL
    work with servers and update login function

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const handleLogin = (event) => {
-  console.log(event);
-};
+const url = "https://1f9dd58e-88c5-46df-854d-280aa21a799b.mock.pstmn.io/api/v1/accounts/login";
+var form = document.querySelector(".form");
 
-let form = document.querySelector(".form");
+function handleLogin(event) {
+  event.preventDefault();
+  const email = form.email.value;
+  const password = form.password.value;
+  const data = {
+    username: form.email.value,
+    password: form.password.value
+  };
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(Respone => Respone.json())
+  .then(data => console.log(data.username));
+  // if(false){
+  //   window.location.href = "personal.html";
+  // }
+  // else{
+  //   alert("Invalid Credentials");
+  // }
+}
 
-form.addEventListener("submit", e => {
-  console.log(document.getElementsByName("email"));
-  e.preventDefault();
-});
+
+form.addEventListener("submit", handleLogin);
