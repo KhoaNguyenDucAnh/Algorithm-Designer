2845261abb4a518d9ebce9bb9f4b3c4d8ef142fa
KhoaNguyenDucAnh <robinkhoa06@gmail.com>
2023-03-25 09:23:27 +0700

    Add sign-up/js/script.js

--- sign-up/js/script.js
+++ sign-up/js/script.js
@@ -1,10 +1,32 @@
-const handleLogin = (event) => {
-  console.log(event);
-};
+const url = "http://localhost:8080/api/v1/accounts/";
+var form = document.querySelector(".form");
 
-let form = document.querySelector(".form");
+function handleLogin(event) {
+  event.preventDefault();
+  const data = {
+    username: form.email.value,
+    password: form.password.value
+  };
 
-form.addEventListener("submit", e => {
-  console.log(document.getElementsByName("email"));
-  e.preventDefault();
-});
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      alert("Invalid Credentials");
+      throw new Error("Invalid Credentials");
+    }
+    return response.json();
+  })
+  .then(function(data) {
+      console.log(data);
+      sessionStorage.setItem("username", data.username);
+      window.location.href = "account.html";
+  });
+}
+
+form.addEventListener("submit", handleLogin);
